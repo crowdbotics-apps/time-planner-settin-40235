85b247d5c900e89ec52ec2fc92a98bcd84bae598
Crowdbotics <team@crowdbotics.com>
2023-04-26 14:39:34 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "time-planner-settin-40235",
+  url: "https://time-planner-settin-40235.botics.co",
+  api: "https://time-planner-settin-40235.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
